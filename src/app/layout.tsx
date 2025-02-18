a8ee321d1dc282af765e445c7b898d89eb80eaaa
Sandro <58832806+sadaquisa@users.noreply.github.com>
2025-02-18 07:20:38 -0500

    feat: add components button primereact

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { PrimeReactProvider } from "primereact/api";
+import Tailwind from "primereact/passthrough/tailwind";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +26,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <PrimeReactProvider value={{ unstyled: true, pt: Tailwind }}>
+          {children}
+        </PrimeReactProvider>
       </body>
     </html>
   );
